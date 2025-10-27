ea9bb1101ce3e7923344c3dab5eede60548ea552
fareedgul <fareedgul735@gmail.com>
2025-10-27 04:01:17 -0700

    push/changes

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import router from "./routes/authRouter.js";
 import cors from "cors";
 import sendOtpRouter from "./routes/sendOtpRouter.js";
 import forgetPasswordRouter from "./routes/forgetPasswordRouter.js";
-import uploadFilesRouter from "./routes/reportRoutes.js";
 
 const app = express();
 
@@ -13,7 +12,6 @@ app.use("/api/auth", router);
 app.use("/api/security", sendOtpRouter);
 app.use("/api/auth", forgetPasswordRouter);
 
-app.use("/uploads", express.static("uploads"));
-app.use("/api/reports", uploadFilesRouter);
+
 
 export default app;
