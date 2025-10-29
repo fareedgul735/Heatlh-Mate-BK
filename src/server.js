6bc9393fc02dc93efa24c0f8c0e41709cf01011e
fareedgul <fareedgul735@gmail.com>
2025-10-29 12:52:50 -0700

    push/changes

--- src/server.js
+++ src/server.js
@@ -7,8 +7,9 @@ dotenv.config();
 
 const startServer = async () => {
   await connectDB();
-  app.listen(PORT, HOST, () => {
-    console.log(`Server is running on ${HOST}:${PORT}`);
+  const port = process.env.PORT || 5000;
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
   });
 };
 
